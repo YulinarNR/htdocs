a0ff6f35319518c317cf6d655b0a399f1078859b
YulinarNR <yulinarnurrahma@gmail.com>
2021-01-07 17:07:24 +0700

    Tugas Praktikum 6

---
+++ E41191151_Yulinar_Nur Rahmawati_(Praktikum_6)/Praktikum_6/belajar_php/operator.php
@@ -0,0 +1,34 @@
+<?php
+$a = 5;
+$b = 2;
+
+//penjumlahan
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr>";
+
+//pengurangan
+$c = $a - $b;
+echo "$a - $b = $c";
+echo "<hr>";
+
+//perkalian
+$c = $a * $b;
+echo "$a * $b = $c";
+echo "<hr>";
+
+//pembagian
+$c = $a / $b;
+echo "$a / $b = $c";
+echo "<hr>";
+
+//SSisa bagi
+$c = $a % $b;
+echo "$a % $b = $c";
+echo "<hr>";
+
+//Pangkat
+$c = $a ** $b;
+echo "$a ** $b = $c";
+echo "<hr>";
+?>
