3d41033376f2b0c917d94126f0f0a26ef9492c8d
YulinarNR <yulinarnurrahma@gmail.com>
2021-01-09 01:22:03 +0700

    database

--- ebusi2/database/db_kebun27.sql
+++ ebusi2/database/db_kebun27.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2021 at 03:56 AM
+-- Generation Time: Jan 08, 2021 at 07:20 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -37,9 +37,9 @@ CREATE TABLE `tb_admin` (
   `nama_belakang` varchar(50) NOT NULL,
   `alamat` text NOT NULL,
   `info_kontak` varchar(50) NOT NULL,
-  `foto_admin` varchar(100) NOT NULL,
+  `foto_admin` varchar(50) NOT NULL,
   `tgl_buat` date NOT NULL,
-  `id_level` int(11) NOT NULL
+  `id_level` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -47,7 +47,7 @@ CREATE TABLE `tb_admin` (
 --
 
 INSERT INTO `tb_admin` (`id_admin`, `email`, `username`, `password`, `nama_depan`, `nama_belakang`, `alamat`, `info_kontak`, `foto_admin`, `tgl_buat`, `id_level`) VALUES
-(3, 'yulinar@gmail.com', 'smkn', 'yulinar', 'Yulinar Nur', 'Rahmawati', 'Probolinggo', '089322234', '530688.jpg', '2020-12-27', 3),
+(3, 'yulinar@gmail.com', 'superadmin', 'yulinar', 'Super', 'Admin', 'Probolinggo', '089322234', '530688.jpg', '2020-12-27', 3),
 (4, 'yulinarnurrahma@gmail.com', 'admin12345', 'admin', 'paperplane', 'bookstore', 'Surabaya', '089423411', '202012290519051.PNG', '2020-12-29', 1);
 
 -- --------------------------------------------------------
@@ -57,7 +57,7 @@ INSERT INTO `tb_admin` (`id_admin`, `email`, `username`, `password`, `nama_depan
 --
 
 CREATE TABLE `tb_bank` (
-  `id_bank` int(11) NOT NULL,
+  `id_bank` int(8) NOT NULL,
   `an_bank` varchar(30) NOT NULL,
   `nama_bank` varchar(30) NOT NULL,
   `no_rek` varchar(15) NOT NULL
@@ -77,8 +77,8 @@ INSERT INTO `tb_bank` (`id_bank`, `an_bank`, `nama_bank`, `no_rek`) VALUES
 --
 
 CREATE TABLE `tb_kategori` (
-  `id_kategori` int(11) NOT NULL,
-  `nama` varchar(100) NOT NULL,
+  `id_kategori` int(5) NOT NULL,
+  `nama` varchar(25) NOT NULL,
   `tgl_input` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -90,7 +90,8 @@ INSERT INTO `tb_kategori` (`id_kategori`, `nama`, `tgl_input`) VALUES
 (12, 'Buah', '2020-12-27'),
 (13, 'Sayuran', '2020-12-27'),
 (14, 'Kacang Kacangan', '2020-12-27'),
-(15, 'Biji Bijian', '2020-12-30');
+(15, 'Biji Bijian', '2020-12-30'),
+(16, 'Batang', '2021-01-08');
 
 -- --------------------------------------------------------
 
@@ -120,7 +121,7 @@ INSERT INTO `tb_level` (`id_level`, `level`) VALUES
 
 CREATE TABLE `tb_ongkir` (
   `id_ongkir` int(11) NOT NULL,
-  `nama_kota` varchar(100) NOT NULL,
+  `nama_kota` varchar(50) NOT NULL,
   `tarif` int(11) NOT NULL,
   `tgl_input` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -132,7 +133,10 @@ CREATE TABLE `tb_ongkir` (
 INSERT INTO `tb_ongkir` (`id_ongkir`, `nama_kota`, `tarif`, `tgl_input`) VALUES
 (4, 'probolinggo', 12000, '2020-12-07'),
 (5, 'Banyuwangi', 24000, '2020-12-27'),
-(6, 'Malang', 27000, '2020-12-27');
+(6, 'Malang', 27000, '2020-12-27'),
+(7, 'Surabaya', 15000, '2021-01-08'),
+(8, 'Kraksaan', 17000, '2021-01-08'),
+(9, 'Solo', 28000, '2021-01-08');
 
 -- --------------------------------------------------------
 
@@ -143,11 +147,11 @@ INSERT INTO `tb_ongkir` (`id_ongkir`, `nama_kota`, `tarif`, `tgl_input`) VALUES
 CREATE TABLE `tb_pembayaran` (
   `id_pembayaran` int(11) NOT NULL,
   `id_pembelian` int(11) NOT NULL,
-  `nama` varchar(250) NOT NULL,
-  `bank` varchar(250) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `bank` varchar(30) NOT NULL,
   `jumlah` int(11) NOT NULL,
   `tgl_pembayaran` date NOT NULL,
-  `bukti_pembayaran` varchar(250) NOT NULL
+  `bukti_pembayaran` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -155,7 +159,11 @@ CREATE TABLE `tb_pembayaran` (
 --
 
 INSERT INTO `tb_pembayaran` (`id_pembayaran`, `id_pembelian`, `nama`, `bank`, `jumlah`, `tgl_pembayaran`, `bukti_pembayaran`) VALUES
-(11, 88, 'Yulinar', 'Mandiri', 41000, '2021-01-01', '202101010600163.PNG');
+(11, 88, 'Yulinar', 'Mandiri', 41000, '2021-01-01', '20210103190643struk3242.jpg'),
+(12, 90, 'Yulinar Nur Rahmawati', 'Mandiri', 77000, '2021-01-03', '20210103190643struk.jpg'),
+(13, 91, 'Itska Muezaa', 'Mandiri', 20000, '2021-01-04', '20210104172301new.jpg'),
+(14, 94, 'user yulinar nr', 'BCA', 64000, '2021-01-07', '20210107180101algo.png'),
+(15, 93, 'Hilmaya', 'Mandiri', 29000, '2021-01-07', '20210107180515lg.jpg');
 
 -- --------------------------------------------------------
 
@@ -169,11 +177,11 @@ CREATE TABLE `tb_pembelian` (
   `id_ongkir` int(11) NOT NULL,
   `tanggal_pembelian` date NOT NULL,
   `total_pembelian` int(11) NOT NULL,
-  `nama_kota` varchar(100) NOT NULL,
+  `nama_kota` varchar(50) NOT NULL,
   `tarif` int(11) NOT NULL,
   `alamat_pengiriman` text NOT NULL,
-  `status_pembelian` varchar(100) NOT NULL DEFAULT 'pending',
-  `resi_pengiriman` varchar(50) NOT NULL
+  `status_pembelian` varchar(30) NOT NULL DEFAULT 'pending',
+  `resi_pengiriman` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -181,8 +189,16 @@ CREATE TABLE `tb_pembelian` (
 --
 
 INSERT INTO `tb_pembelian` (`id_pembelian`, `id_user`, `id_ongkir`, `tanggal_pembelian`, `total_pembelian`, `nama_kota`, `tarif`, `alamat_pengiriman`, `status_pembelian`, `resi_pengiriman`) VALUES
-(88, 12, 5, '2021-01-01', 41000, 'Banyuwangi', 24000, 'banyu iki', 'Sudah Kirim Pembayaran', ''),
-(89, 12, 6, '2021-01-01', 51000, 'Malang', 27000, 'malabf', 'Pesanan Dibatalkan', '');
+(88, 12, 5, '2021-01-01', 41000, 'Banyuwangi', 24000, 'banyu iki', 'SELESAI', 'KS-23129324211'),
+(89, 12, 6, '2021-01-01', 51000, 'Malang', 27000, 'malabf', 'Pesanan Dibatalkan', ''),
+(90, 12, 6, '2021-01-03', 77000, 'Malang', 27000, 'Jl. Ir Soekarno no 16', 'SELESAI', 'KB-11342343212'),
+(91, 12, 4, '2021-01-04', 20000, 'probolinggo', 12000, 'a', 'Sudah Kirim Pembayaran', ''),
+(92, 12, 4, '2021-01-07', 26000, 'probolinggo', 12000, 'Jl. Cangkring, Kanigaran', 'Pesanan Dibatalkan', ''),
+(93, 12, 5, '2021-01-07', 29000, 'Banyuwangi', 24000, 'Jl. Kawah', 'Sudah Kirim Pembayaran', ''),
+(94, 12, 6, '2021-01-07', 64000, 'Malang', 27000, 'Jl. ketaman sari', 'Sudah Kirim Pembayaran', ''),
+(95, 12, 5, '2021-01-07', 42000, 'Banyuwangi', 24000, 'Jl. ajdaks', 'Pesanan Dibatalkan', ''),
+(96, 12, 5, '2021-01-07', 27000, 'Banyuwangi', 24000, 'sad', 'Pesanan Dibatalkan', ''),
+(97, 12, 9, '2021-01-08', 41000, 'Solo', 28000, 'Jl. Mawar', 'pending', '');
 
 -- --------------------------------------------------------
 
@@ -195,7 +211,7 @@ CREATE TABLE `tb_pembelian_produk` (
   `id_pembelian` int(11) NOT NULL,
   `id_produk` int(11) NOT NULL,
   `jumlah` int(11) NOT NULL,
-  `nama` varchar(100) NOT NULL,
+  `nama` varchar(25) NOT NULL,
   `harga` int(11) NOT NULL,
   `berat` int(11) NOT NULL,
   `subberat` int(11) NOT NULL,
@@ -209,53 +225,20 @@ CREATE TABLE `tb_pembelian_produk` (
 INSERT INTO `tb_pembelian_produk` (`id_pembelian_produk`, `id_pembelian`, `id_produk`, `jumlah`, `nama`, `harga`, `berat`, `subberat`, `subtot`) VALUES
 (122, 88, 29, 1, 'Selada', 12000, 1, 1, 12000),
 (123, 88, 30, 1, 'Sawi', 5000, 1, 1, 5000),
-(124, 89, 29, 2, 'Selada', 12000, 1, 2, 24000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_pengaturan`
---
-
-CREATE TABLE `tb_pengaturan` (
-  `id_pengaturan` int(11) NOT NULL,
-  `judul` varchar(50) NOT NULL,
-  `moto` varchar(255) NOT NULL,
-  `gambar_awal` varchar(50) NOT NULL,
-  `judul_tentang` varchar(50) NOT NULL,
-  `gambar_tentang` varchar(50) NOT NULL,
-  `text` text NOT NULL,
-  `info_tambahan` varchar(255) NOT NULL,
-  `galeri` varchar(25) NOT NULL,
-  `judul_service` varchar(50) NOT NULL,
-  `service1` varchar(25) NOT NULL,
-  `gambar_sv1` varchar(25) NOT NULL,
-  `text_sv1` varchar(100) NOT NULL,
-  `service2` varchar(25) NOT NULL,
-  `gambar_sv2` varchar(25) NOT NULL,
-  `text_sv2` varchar(100) NOT NULL,
-  `service3` varchar(25) NOT NULL,
-  `gambar_sv3` varchar(25) NOT NULL,
-  `text_sv3` varchar(100) NOT NULL,
-  `judul_kontak` varchar(25) NOT NULL,
-  `judul_alamat` varchar(50) NOT NULL,
-  `nama_jalan` varchar(100) NOT NULL,
-  `nama_kota_kab` varchar(100) NOT NULL,
-  `link_maps` text NOT NULL,
-  `judul_email` varchar(50) NOT NULL,
-  `email1` varchar(50) NOT NULL,
-  `email2` varchar(50) NOT NULL,
-  `judul_call` varchar(50) NOT NULL,
-  `no_hp1` varchar(12) NOT NULL,
-  `no_hp2` varchar(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_pengaturan`
---
-
-INSERT INTO `tb_pengaturan` (`id_pengaturan`, `judul`, `moto`, `gambar_awal`, `judul_tentang`, `gambar_tentang`, `text`, `info_tambahan`, `galeri`, `judul_service`, `service1`, `gambar_sv1`, `text_sv1`, `service2`, `gambar_sv2`, `text_sv2`, `service3`, `gambar_sv3`, `text_sv3`, `judul_kontak`, `judul_alamat`, `nama_jalan`, `nama_kota_kab`, `link_maps`, `judul_email`, `email1`, `email2`, `judul_call`, `no_hp1`, `no_hp2`) VALUES
-(1, 'SISTEM INFORMASI E-BUSI', 'Cepat Mudah Efektif', 'jpg', 'Kebun Inovasi Polije', 'jpg', 'Kebun Inovasi bertujuan untuk menciptakan sumber daya manusia yang berkualitas. Beragam koleksi tanaman yang tersedia di Kebun Inovasi adalah semangka, melon, terong, bawang merah, jagung dan juga tanaman jeruk. Kegiatan pengelolaan lahan ini juga melibatkan instruktur yang ahli di bidang pertanian. Instruktur pertanian akan melakukan edukasi materi dan praktik langsung di lahan ini. Di antara materi yang akan diberikan yakni\r\n', 'Cara bercocok tanam di lahan kering dan basah\r\nCara perawatan tanaman\r\nInstruktur pertanian juga akan mengajarkan proses mencampurkan pupuk organi', 'jpg', 'SERVICES', 'PELAYANAN TERBAIK', 'jpg', 'Respon yang cepat dan aman\r\nBerbelanja nyaman', 'KUALITAS TERBAIK', 'jpg', 'Kualitas sudah terjamin  Pesanan aman sampai tujuan', 'BUAH DAN SAYURAN SEGAR', 'jpg', ' Buah/sayur segar dan bersih\r\n Langsung petik dari kebun', 'KONTAK', 'Our Address', 'Jl. Mastrip Kotak Pos 164', 'Jember, Jawa Timur, Indonesia', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4696.704327963119!2d113.72202653559624!3d-8.15816096065096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd695b617d8f623%3A0xf6c4437632474338!2sState%20Polytechnic%20of%20Jember!5e0!3m2!1sen!2sid!4v1605834966884!5m2!1sen!2sid', 'Email Us', 'ujang_tri@polije.ac.id', 'tosan@polije.ac.id', 'Call Us', '08124944360', '081335703958');
+(124, 89, 29, 2, 'Selada', 12000, 1, 2, 24000),
+(125, 90, 29, 2, 'Selada', 8000, 1, 2, 16000),
+(126, 90, 32, 1, 'Jeruk', 34000, 2, 2, 34000),
+(127, 91, 29, 1, 'Selada', 8000, 1, 1, 8000),
+(128, 92, 29, 1, 'Selada', 8000, 1, 1, 8000),
+(129, 92, 31, 2, 'Bayam', 3000, 1, 2, 6000),
+(130, 93, 30, 1, 'Sawi', 5000, 1, 1, 5000),
+(131, 94, 31, 1, 'Bayam', 3000, 1, 1, 3000),
+(132, 94, 32, 1, 'Jeruk', 34000, 2, 2, 34000),
+(133, 95, 29, 1, 'Selada', 8000, 1, 1, 8000),
+(134, 95, 30, 2, 'Sawi', 5000, 1, 2, 10000),
+(135, 96, 31, 1, 'Bayam', 3000, 1, 1, 3000),
+(136, 97, 29, 1, 'Selada', 8000, 1, 1, 8000),
+(137, 97, 30, 1, 'Sawi', 5000, 1, 1, 5000);
 
 -- --------------------------------------------------------
 
@@ -268,9 +251,9 @@ CREATE TABLE `tb_produk` (
   `nama_produk` varchar(50) NOT NULL,
   `kategori` varchar(25) NOT NULL,
   `stok` int(11) NOT NULL,
-  `beratisi_produk` varchar(50) NOT NULL,
+  `beratisi_produk` varchar(20) NOT NULL,
   `harga_produk` int(11) NOT NULL,
-  `foto_produk` varchar(100) NOT NULL,
+  `foto_produk` varchar(25) NOT NULL,
   `info_produk` text NOT NULL,
   `nutrisi_manfaat` text NOT NULL,
   `cara_penyimpanan` text NOT NULL
@@ -281,10 +264,10 @@ CREATE TABLE `tb_produk` (
 --
 
 INSERT INTO `tb_produk` (`id_produk`, `nama_produk`, `kategori`, `stok`, `beratisi_produk`, `harga_produk`, `foto_produk`, `info_produk`, `nutrisi_manfaat`, `cara_penyimpanan`) VALUES
-(29, 'Selada', '13', 9, '1', 8000, 'selada.jpg', 'Selada atau daun sla adalah tumbuhan sayur yang biasa ditanam di daerah beriklim sedang maupun daerah tropika. Kegunaan utama adalah sebagai salad. ', 'Mencegah dehidrasi, Menangkal radikal bebas, Menjaga kesehatan mata, Menjaga kesehatan tulang, Membantu menurunkan tekanan darah dan masih banyak lagi', 'Melansir laman The Spruce, kamu harus memisahkan daun-daun selada terlebih dahulu, cuci bersih, dan keringkan menggunakan salad spinner.\r\nSetelah itu, bungkus menggunakan kertas tipis atau tisu dan letakkan dalam penyimpanan sayur yang ada di lemari pendingin.\r\nDengan metode ini, selada akan awet selama 3-5 hari.'),
-(30, 'Sawi', '13', 19, '1', 5000, '530688.jpg', 'xx', 'xx', 'xx'),
-(31, 'Bayam', '13', 10, '1', 3000, 'bayam2.jpg', 'xx', 'xx', 'xx'),
-(32, 'Jeruk', '12', 23, '2', 34000, 'jeruk.jpg', 'xx', 'xx', 'xx');
+(29, 'Selada', '13', 5, '1', 8000, 'selada.jpg', 'Selada atau daun sla adalah tumbuhan sayur yang biasa ditanam di daerah beriklim sedang maupun daerah tropika. Kegunaan utama adalah sebagai salad. ', 'Mencegah dehidrasi, Menangkal radikal bebas, Menjaga kesehatan mata, Menjaga kesehatan tulang, Membantu menurunkan tekanan darah dan masih banyak lagi', 'Melansir laman The Spruce, kamu harus memisahkan daun-daun selada terlebih dahulu, cuci bersih, dan keringkan menggunakan salad spinner.\r\nSetelah itu, bungkus menggunakan kertas tipis atau tisu dan letakkan dalam penyimpanan sayur yang ada di lemari pendingin.\r\nDengan metode ini, selada akan awet selama 3-5 hari.'),
+(30, 'Sawi', '13', 15, '1', 5000, '530688.jpg', 'xx', 'xx', 'xx'),
+(31, 'Bayam', '13', 7, '1', 3000, 'bayam2.jpg', 'xx', 'xx', 'xx'),
+(32, 'Jeruk', '12', 21, '2', 34000, 'jeruk.jpg', 'xx', 'xx', 'xx');
 
 -- --------------------------------------------------------
 
@@ -295,15 +278,15 @@ INSERT INTO `tb_produk` (`id_produk`, `nama_produk`, `kategori`, `stok`, `berati
 CREATE TABLE `tb_user` (
   `id_user` int(5) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(25) NOT NULL,
   `nama_depan` varchar(50) NOT NULL,
   `nama_belakang` varchar(50) NOT NULL,
   `alamat` text NOT NULL,
-  `info_kontak` varchar(100) NOT NULL,
-  `foto_user` varchar(100) NOT NULL,
+  `info_kontak` varchar(25) NOT NULL,
+  `foto_user` varchar(25) NOT NULL,
   `tgl_buat` date NOT NULL,
-  `id_level` int(2) NOT NULL
+  `id_level` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -366,12 +349,6 @@ ALTER TABLE `tb_pembelian_produk`
   ADD PRIMARY KEY (`id_pembelian_produk`);
 
 --
--- Indexes for table `tb_pengaturan`
---
-ALTER TABLE `tb_pengaturan`
-  ADD PRIMARY KEY (`id_pengaturan`);
-
---
 -- Indexes for table `tb_produk`
 --
 ALTER TABLE `tb_produk`
@@ -397,13 +374,13 @@ ALTER TABLE `tb_admin`
 -- AUTO_INCREMENT for table `tb_bank`
 --
 ALTER TABLE `tb_bank`
-  MODIFY `id_bank` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_bank` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_kategori` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `tb_level`
@@ -415,43 +392,37 @@ ALTER TABLE `tb_level`
 -- AUTO_INCREMENT for table `tb_ongkir`
 --
 ALTER TABLE `tb_ongkir`
-  MODIFY `id_ongkir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_ongkir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tb_pembayaran`
 --
 ALTER TABLE `tb_pembayaran`
-  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tb_pembelian`
 --
 ALTER TABLE `tb_pembelian`
-  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 
 --
 -- AUTO_INCREMENT for table `tb_pembelian_produk`
 --
 ALTER TABLE `tb_pembelian_produk`
-  MODIFY `id_pembelian_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
-
---
--- AUTO_INCREMENT for table `tb_pengaturan`
---
-ALTER TABLE `tb_pengaturan`
-  MODIFY `id_pengaturan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pembelian_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=138;
 
 --
 -- AUTO_INCREMENT for table `tb_produk`
 --
 ALTER TABLE `tb_produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_user` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
